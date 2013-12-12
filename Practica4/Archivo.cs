e4a6dacb3f46f1501c5462ff4634a7ec6335d544
EmmaChuy <emanuel_chino13@hotmail.com>
2013-12-11 18:56:28 -0600

    Practica 4

---
+++ Practica4/Archivo.cs
@@ -0,0 +1,92 @@
+﻿using System.IO;
+using System;
+
+namespace Practica4
+{
+
+	public class Archivo
+	{
+		public Archivo(){}//constructor
+	private string path;
+	
+	public Archivo(String path)
+	{
+	this.path = path;
+	
+	}
+	public void setPath(String path)
+	{
+	
+		Console.Write("Dame la URL del Archivo");
+		path = Console.ReadLine();
+		
+		this.path = path;
+	}
+	public void leer()
+	{
+	
+	String linea;
+
+	try{
+
+	//Se indica el archivo a leer
+	StreamReader lector = new StreamReader(this.path);
+	
+	//lee la primera linea del archivo
+	linea = lector.ReadLine();
+	//continua leyendo hasta que se termina el archivo
+	while (linea != null){
+	
+	//Escribe la linea en la consola
+	Console.WriteLine(linea);
+	//lee la siguiente linea 
+	linea  = lector.ReadLine();
+	
+	}
+	//cierra el lector 
+	lector.Close();
+	Console.ReadLine();
+		}
+
+
+catch(FileNotFoundException )
+{
+
+	Console.WriteLine("El Archivo no existe!!");	
+
+}
+
+	finally
+	{
+		Console.WriteLine("Cerrando la  lectura");
+	}
+	}
+	public void escribir (String [] contenido){
+	
+		try{
+		
+		//se indica el archivo a leer
+		StreamWriter escritor = new StreamWriter(this.path);
+		
+		foreach(String linea in contenido){
+			
+			//Escriba una linea
+			escritor.WriteLine(linea);
+		
+		}
+		//Cierra el escritor
+		escritor.Close();
+		
+		}
+		catch (Exception e)
+		{
+			Console.WriteLine("Exception: "+e.Message);
+		
+		}
+		finally
+		{
+			Console.WriteLine("Cerrando la Escritura");
+		}
+	  }
+	}
+}
---
+++ Practica4/Principal.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+
+namespace Practica4
+{
+
+	public class Principal
+	{
+		static void Main (string [] args){
+		
+		string URL = "";
+			Archivo file = new Archivo();
+			file.setPath(URL);
+			
+			file.leer();
+			Console.ReadKey();
+		
+		}
+	}
+}
